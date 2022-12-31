747c67c18a5ba6c4d13b83b346b3bd2d5769db60
grantmwilson <development@grantmwilson.com>
2022-12-31 15:34:35 -0500

    Added version check to github actions

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,6 +1,6 @@
-from ..client import Client
+from pyaccord import Client
 
 
-def test_sanity():
+def test_client_sanity():
     client = Client("FAKE BOT TOKEN", api_version=10)
     assert client.api_url == "https://discord.com/api/v10"
